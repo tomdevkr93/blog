06dcab665409db79bff1fe3ef717d20f4f22afef
Tom <tom.dev.kr@gmail.com>
2024-08-23 18:29:27 +0900

    base path 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tomdevkr93.github.io/',
-  base: '/',
+  site: 'https://tomdevkr93.github.io',
+  base: '/blog',
   trailingSlash: 'always',
   integrations: [
     tailwind(),
