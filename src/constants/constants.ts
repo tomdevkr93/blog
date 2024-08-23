eca677fb6e4d8fbbcf204172d7783a3c00849c1a
Tom <tom.dev.kr@gmail.com>
2024-08-23 14:52:50 +0900

    기본 light mode

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,4 +5,4 @@ export const PAGE_SIZE = 8
 export const LIGHT_MODE = 'light',
   DARK_MODE = 'dark',
   AUTO_MODE = 'auto'
-export const DEFAULT_THEME = AUTO_MODE
+export const DEFAULT_THEME = LIGHT_MODE
