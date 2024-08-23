b26d2be91094470c22e177d2dd4fee907afd361c
Tom <tom.dev.kr@gmail.com>
2024-08-23 17:26:53 +0900

    레이아웃 색상값 변경

--- src/config.ts
+++ src/config.ts
@@ -11,8 +11,8 @@ export const siteConfig: SiteConfig = {
   subtitle: '',
   lang: 'ko',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
-    hue: 240,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: true,     // Hide the theme color picker for visitors
+    hue: 75,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: false,
